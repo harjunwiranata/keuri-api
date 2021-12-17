c430bca22d1027ca329a7e57fee48f0b0f387442
harjunwiranata <harjunwiranata@gmail.com>
2021-12-17 23:27:24 +0700

    heroku dispatch

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const app = express()
-
-
-app.get('/', (req,res) => {
-    res.send("Hello World")
-})
-
-app.listen(80, () => {
-    console.log('server running')
-})
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
